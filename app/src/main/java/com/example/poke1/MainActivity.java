01664e3ba001fdfaca542e9ed62962df4883bf00
Tanmai2002 <tanmaikamat@gmail.com>
2021-10-24 16:01:30 +0530

    display added

--- app/src/main/java/com/example/poke1/MainActivity.java
+++ app/src/main/java/com/example/poke1/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.poke1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String msg="com.";
+    public static final String msg="com.example.poke1.msg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void butCLicked(View view){
+        Intent i=new Intent(this,display.class);
+        i.putExtra(msg,((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString());
+        startActivity(i);
     }
 }
