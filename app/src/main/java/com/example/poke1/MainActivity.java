fb120d6314df987287bf61ad6f9a770e70b68bd4
Tanmai2002 <tanmaikamat@gmail.com>
2021-10-24 15:52:23 +0530

    firssst commit

---
+++ app/src/main/java/com/example/poke1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.poke1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String msg="com.";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
